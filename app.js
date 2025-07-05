a0be5941a4a8015220ea9c7bf864401b4ae06ab6
Mr Freaks <leezzyixxose@gmail.com>
2025-07-05 22:10:04 +0800

    Numbers and Maths

---
+++ app.js
@@ -0,0 +1,8 @@
+let Celcius = -5;
+const Farenheit = 86;
+
+const ConFarenheit = (Celcius * 9/5 ) + 32;
+const ConCelcius = (Farenheit - 32) * 5 / 9;
+
+console.log(ConFarenheit);
+console.log(ConCelcius);
