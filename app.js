a209d94cd6bf44cab447a54f263f699d5b7540ef
Mr Freaks <leezzyixxose@gmail.com>
2025-07-06 03:52:09 +0800

    Strings Numbers and Math

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/*
+LESSON 2 Numbers and Maths
+
 let Celcius = -5;
 const Farenheit = 86;
 
@@ -6,3 +9,30 @@ const ConCelcius = (Farenheit - 32) * 5 / 9;
 
 console.log(ConFarenheit);
 console.log(ConCelcius);
+
+
+*/
+
+//const text = 'My Name is ';
+//const Name = 'Lee';
+
+const Basketballs = 2095;
+const Shippingfee = 499;
+const tshirts = 799;
+const items = 2;
+
+const shipnhand = (Shippingfee * 2) /100;
+const totalamount = ((Basketballs * items) + (tshirts * items)) / 100;
+const taxcalc = (((Basketballs * items) + (tshirts * items) + (Shippingfee * 2)) * 0.1);
+const totalb4tax = totalamount + shipnhand;
+
+
+const display = `Items (${items * 2}): $` + totalamount + `\n Shipping and Handling: $` + shipnhand + `\n
+Total Before Tax: $${totalb4tax} \nEstimated Tax(10%): $${Math.round((2 * 2095 + 2 * 799 + 499 + 499) * 0.1) / 100}`;
+alert(display);
+//const concatenation = 'Total Cost: $' + calculation;
+
+
+//console.log(interpolation);
+//alert(interpolation);
+
